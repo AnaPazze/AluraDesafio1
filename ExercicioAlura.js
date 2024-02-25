41bc3404cf7b6f6e0ecc0b32ffa0119f0f7592c6
Ana Pazze <112920077+AnaPazze@users.noreply.github.com>
2024-02-24 23:16:22 -0300

    Add files via upload

---
+++ ExercicioAlura.js
@@ -0,0 +1,40 @@
+//exercíco 1 
+diaDaSemana = prompt('Qual é o dia da semana?');
+if(diaDaSemana == 'sábado'){
+    alert('Bom fim de semana');
+
+}else if(diaDaSemana == 'domingo'){
+alert('Bom fim de semana');
+
+}else{
+    alert('Boa semana');
+{
+
+//exercício 2
+    numero = prompt('Digite um positivo ou negativo');
+if (numero > 0) {
+    alert('Número positivo!');
+} else {
+    alert('Número negativo!');
+}
+
+//exercício 3
+
+pontuação = 102;
+if (pontuação > 100) {
+    console.log('Parabéns você venceu!');
+} else {
+    console.log('Tente novamente para você ganhar!');
+}
+
+//exercìcio 4 
+
+let saldoDaConta = 1000;
+alert(`seu saldo da conta é ${saldoDaConta} ` );
+
+//exercicio 5
+
+let SeuNome = prompt('Insira seu nome');
+alert(`Boas vindas!!! ${SeuNome}`);
+
+
