9b4fc566b37598f7a65c46d72829a9bf3b2a6772
Ana Pazze <112920077+AnaPazze@users.noreply.github.com>
2024-02-26 20:42:28 -0300

    Add files via upload

---
+++ functions javascript.js
@@ -0,0 +1,46 @@
+function exibirOlaNoConsole() {
+  console.log("Olá, mundo!");
+
+}
+
+exibirOlaNoConsole();
+
+function exibirOlaNome(nome) {
+  cosole.log(`Olá, ${nome}!`);
+{
+
+exibirOlaNome("Luiza");
+
+
+function MultiplicarDobro(numero) {
+  return numero * 2;
+
+
+}
+
+let resultado = MultiplicarDobro(2);
+console.log(resultado);
+
+
+function CalcularMedia(num1, num2, num3){
+  return (num1 + num2 + num3)/3
+}
+  let media = CalcularMedia(1, 2, 3 );
+  console.log(media);
+
+
+function MaiorNumero(a, b){ 
+  return a> b ? a : b;
+  }
+   et numeroMaior = MaiorNumero(12, 8);
+  console.log(numeroMaior);
+  
+
+   
+   
+function CalcularMultiplicação(numero){
+  return numero * numero;
+   }
+
+  let resultadoMultiplicação = CalcularMultiplicação(2);
+  console.log(resultadoMultiplicação);
